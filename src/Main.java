438be2e3855587413f3220c1c18d68dbbaf74f1f
Marko Ilic <marko.ilic@prodyna.com>
2021-10-15 10:55:10 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Prvi red aplikacije...");
+        System.out.println("Drugi red aplikacije...");
+        System.out.println("Treci red aplikacije...");
+    }
+}
